927a3282d37135f9ca81299cbc9c18dffbd84389
Aaron Rudkin <junk_signups@rudkin.ca>
2025-02-12 13:25:09 +0000

    Fix dockets with greater than 8000 comments.

--- ustr_docket_scraper.py
+++ ustr_docket_scraper.py
@@ -1,3 +1,4 @@
+import pprint
 import os
 import time
 import requests
@@ -29,27 +30,60 @@ def docket_get_count(docket_id):
     d = json.loads(res.text)
     return int(d["actions"][0]["returnValue"]["returnValue"])
 
+def docket_get_offset(docket_id, previous_fsv, previous_fi):
+    print("Checking offset...")
+    previous_fsv = r'\"' + previous_fsv + r'\"' if previous_fsv != "null" and not previous_fsv.startswith(r'\"') else previous_fsv
+
+    data = {
+        'message': '{"actions":[{"id":"84;a","descriptor":"aura://ApexActionController/ACTION$execute","callingDescriptor":"UNKNOWN","params":{"namespace":"","classname":"UstrfbExclusionRequestListController","method":"getOffset","params":{"selectorJSON":"{\\"docketNumber\\":\\"' + docket_id + '\\",\\"filterString\\":\\"\\",\\"sortField\\":\\"Random_URL_Code__c\\",\\"contentFieldReferenceId\\":null,\\"sortDirection\\":\\"asc\\",\\"pageSize\\":\\"50\\",\\"previousFinalSortValue\\":' + previous_fsv + ',\\"previousFinalIndex\\":' + str(previous_fi) + '}"},"cacheable":false,"isContinuation":false}}]}',
+        'aura.context': '{"mode":"PROD","fwuid":"eUNJbjV5czdoejBvRlA5OHpDU1dPd1pMVExBQkpJSlVFU29Ba3lmcUNLWlE5LjMyMC4y","app":"siteforce:communityApp","loaded":{"APPLICATION@markup://siteforce:communityApp":"1184_AgcTXn_6dZSShHXZ2PZsug","MODULE@markup://lightning:f6Controller":"300_MTpiBnsQUPWZH6VdweEpwA","COMPONENT@markup://instrumentation:o11ySecondaryLoader":"343_75unE-CE7CDfRvzL8FM_Uw"},"dn":[],"globals":{},"uad":false}',
+        'aura.pageURI': f'/s/docket?docketNumber={docket_id}',
+        'aura.token': 'null',
+    }
+    res = requests.post('https://comments.ustr.gov/s/sfsites/aura', params=params, data=data, timeout=10)
+    d = json.loads(res.text)
+    try:
+        rv = d["actions"][0]["returnValue"]["returnValue"]
+    except:
+        import pdb
+        import pprint
+        pprint.pprint(data)
+        pdb.set_trace()
+        return -1, -1
+    return rv["finalSortValue"], rv["finalIndex"]
+
 def docket_get_comments(docket_id, max_id):
     results = None
     min_id = 0
+    previous_fsv = "null"
+    previous_fi = "null"
     while min_id < max_id:
         end_id = min(min_id + 50, max_id)
+        offset_min_id = min_id % 8000
+        offset_end_id = end_id % 8000 if end_id % 8000 != 0 else 8000
+        if min_id % 8000 == 0 and min_id:
+            previous_fsv, previous_fi = docket_get_offset(docket_id, previous_fsv, previous_fi)
+            if previous_fi == -1:
+                return
+            previous_fsv = r'\"' + previous_fsv + r'\"' if previous_fsv != "null" and not previous_fsv.startswith('"') else previous_fsv
+            print(previous_fsv)
+
         print(f"\tPage beginning with result {min_id} through to {end_id}")
         data = {
-            'message': '{"actions":[{"id":"85;a","descriptor":"aura://ApexActionController/ACTION$execute","callingDescriptor":"UNKNOWN","params":{"namespace":"","classname":"UstrfbExclusionRequestListController","method":"getPageRecords","params":{"selectorJSON":"{\\"docketNumber\\":\\"' + docket_id + '\\",\\"filterString\\":\\"\\",\\"sortField\\":\\"Random_URL_Code__c\\",\\"contentFieldReferenceId\\":null,\\"sortDirection\\":\\"asc\\",\\"pageSize\\":\\"50\\",\\"previousFinalSortValue\\":null,\\"previousFinalIndex\\":null,\\"startIndex\\":' + str(min_id) + ',\\"endIndex\\":' + str(end_id) + '}"},"cacheable":false,"isContinuation":false}}]}',
+            'message': '{"actions":[{"id":"85;a","descriptor":"aura://ApexActionController/ACTION$execute","callingDescriptor":"UNKNOWN","params":{"namespace":"","classname":"UstrfbExclusionRequestListController","method":"getPageRecords","params":{"selectorJSON":"{\\"docketNumber\\":\\"' + docket_id + '\\",\\"filterString\\":\\"\\",\\"sortField\\":\\"Random_URL_Code__c\\",\\"contentFieldReferenceId\\":null,\\"sortDirection\\":\\"asc\\",\\"pageSize\\":\\"50\\",\\"previousFinalSortValue\\":' + previous_fsv + ',\\"previousFinalIndex\\":' + str(previous_fi) + ',\\"startIndex\\":' + str(offset_min_id) + ',\\"endIndex\\":' + str(offset_end_id) + '}"},"cacheable":false,"isContinuation":false}}]}',
             'aura.context': '{"mode":"PROD","fwuid":"eUNJbjV5czdoejBvRlA5OHpDU1dPd1pMVExBQkpJSlVFU29Ba3lmcUNLWlE5LjMyMC4y","app":"siteforce:communityApp","loaded":{"APPLICATION@markup://siteforce:communityApp":"1184_AgcTXn_6dZSShHXZ2PZsug","MODULE@markup://lightning:f6Controller":"300_MTpiBnsQUPWZH6VdweEpwA","COMPONENT@markup://instrumentation:o11ySecondaryLoader":"343_75unE-CE7CDfRvzL8FM_Uw"},"dn":[],"globals":{},"uad":false}',
             'aura.pageURI': f'/s/docket?docketNumber={docket_id}',
             'aura.token': 'null',
         }
         error_count = 0
-        while error_count < 4:
+        while error_count < 20:
             try:
-                res = requests.post('https://comments.ustr.gov/s/sfsites/aura', params=params, data=data, timeout=3)
+                res = requests.post('https://comments.ustr.gov/s/sfsites/aura', params=params, data=data, timeout=20)
                 error_count = 0
                 break
             except:
                 print("Timeout!")
-                time.sleep(5)
+                time.sleep(5 * (error_count + 1))
             error_count = error_count + 1
         if error_count:
             print(f"DID NOT COMPLETE, stopped at {min_id}")
@@ -63,6 +97,8 @@ def docket_get_comments(docket_id, max_id):
             results = pd.concat([results, rdf])
         else:
             results = rdf
+
+        print(f"Total comments gathered is now {results.shape[0]}")
 
         min_id = min_id + 50
 
@@ -90,7 +126,10 @@ def do_all_dockets(dockets):
         all_comments_from_docket(x)
 
 def all_comments_from_docket(docket_id):
-    df = pd.read_csv(f"{docket_id}.csv")
+    try:
+        df = pd.read_csv(f"{docket_id}.csv")
+    except:
+        return
     print(f"Pulling comments for docket {docket_id}")
     os.makedirs(f"dockets/{docket_id}", exist_ok=True)
     for comment_id in tqdm.tqdm(df["Random_URL_Code__c"]):
@@ -108,8 +147,11 @@ def pull_comment(docket_id, comment_id):
         'aura.token': 'null',
     }
 
-    res = requests.post('https://comments.ustr.gov/s/sfsites/aura', params=p2, data=data, timeout=30)
-    d = json.loads(res.text)
+    try:
+        res = requests.post('https://comments.ustr.gov/s/sfsites/aura', params=p2, data=data, timeout=30)
+        d = json.loads(res.text)
+    except:
+        return
 
     with open(f"dockets/{docket_id}/{comment_id}.json", "w") as outfile:
         json.dump(d["actions"][0]["returnValue"]["returnValue"], outfile)
